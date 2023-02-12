10e0b926913591336aba4af6a135fd23fc184613
perambharathteja <118559257+perambharathteja@users.noreply.github.com>
2023-02-12 16:37:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let contactUSEl = document.getElementById("contactUS");
+let statusEl = document.getElementById("status");
+
+contactUSEl.onclick = function() {
+    statusEl.textContent = "We will reach you ofter some time!";
+    statusEl.style.color = "orange";
+    statusEl.style.fontSize = "24px";
+    statusEl.style.fontWeight = "bold";
+}
